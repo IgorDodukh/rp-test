ca21b9a4b7d67c4ff0255af3d26a146e4d1bcd8d
IgorDodukh <ihor.dodukh@gmail.com>
2025-02-13 20:33:03 +0000

    test code checker

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, PlaywrightTestConfig } from '@playwright/test';
+import { PlaywrightTestConfig } from '@playwright/test';
 
 // export default defineConfig({
 //   testDir: './tests',
@@ -9,29 +9,28 @@ import { defineConfig, PlaywrightTestConfig } from '@playwright/test';
 //   },
 // });
 
-const RPconfig = {
-    apiKey: 'playwright__5iMyhS2TcGmvL7TL3pdxrR127B02tlKf3VRVAaDVh0QFxmfJrexdPbWMcQaz4PY',
-    endpoint: 'http://localhost:8080/api/v1',
-    project: 'default_personal',
-    launch: 'local-28-01-2025-1',
-    attributes: [
-      {
-        key: 'agent',
-        value: 'playwright',
-        },
-      {
-        value: 'demo',
-      },
-    ],
-    description: 'This is an example launch with playwright tests',
-    includeTestSteps: true,
-    skippedIssue: false,  
-  };
+// const RPconfig = {
+//     apiKey: 'playwright__5iMyhS2TcGmvL7TL3pdxrR127B02tlKf3VRVAaDVh0QFxmfJrexdPbWMcQaz4PY',
+//     endpoint: 'http://localhost:8080/api/v1',
+//     project: 'default_personal',
+//     launch: 'local-28-01-2025-1',
+//     attributes: [
+//       {
+//         key: 'agent',
+//         value: 'playwright',
+//         },
+//       {
+//         value: 'demo',
+//       },
+//     ],
+//     description: 'This is an example launch with playwright tests',
+//     includeTestSteps: true,
+//     skippedIssue: false,  
+//   };
 
   const config: PlaywrightTestConfig = {
     reporter: [["line"], ["allure-playwright"]],
 
-    // reporter: [['@reportportal/agent-js-playwright', RPconfig]],
     testDir: './tests',
     timeout: 30000,
     retries: 1,
